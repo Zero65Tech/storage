49a3918d97ab5fdf7e258c798926c565beb590f8
Prashant Gupta <antshpra@gmail.com>
2023-12-24 16:46:37 +0530

    mock exports.createReadStream & exports.createWriteStream

--- src/index.js
+++ src/index.js
@@ -3,34 +3,43 @@ const { Storage } = require('@google-cloud/storage');
 
 
 
-async function ensureLocalPathDir(localPath) {
-  let i = localPath.lastIndexOf('/');
-  if(i != -1) {
-    let dir = localPath.substring(0, i);
-    if(!fs.existsSync(dir))
-      await fs.promises.mkdir(dir, { recursive: true });
-  }
-}
-
-
-
 exports.init = ({ bucket, mock }) => {
+
+  function local(remotePath) {
+    return `@storage/${ bucket }/${ remotePath }`
+  }
+
+  async function ensureLocalPathDir(localPath) {
+    let i = localPath.lastIndexOf('/');
+    if(i != -1) {
+      let dir = localPath.substring(0, i);
+      if(!fs.existsSync(dir))
+        await fs.promises.mkdir(dir, { recursive: true });
+    }
+  }
 
   if(mock === true) {
 
-    exports.sync = async (remotePath) => {
-      return `@storage/${ bucket }/${ remotePath }`;
-    }
-
     exports.readFile = async (remotePath) => {
-      let localPath = `@storage/${ bucket }/${ remotePath }`;
+      let localPath = local(remotePath);
       return await fs.promises.readFile(localPath);
     }
 
     exports.writeFile = async (remotePath, data) => {
-      let localPath = `@storage/${ bucket }/${ remotePath }`;
+      let localPath = local(remotePath);
       await ensureLocalPathDir(localPath);
       return await fs.promises.writeFile(localPath, data);
+    }
+
+    exports.createReadStream = (remotePath) => {
+      let localPath = local(remotePath);
+      return fs.createReadStream(localPath);
+    }
+
+    exports.createWriteStream = (remotePath) => {
+      let localPath = local(remotePath);
+      ensureLocalPathDir(localPath);
+      return fs.createWriteStream(localPath);
     }
 
   } else {
