75d617d2bb5f8a24bf3e36b91cd4932fdd2c9672
jaigupta04 <jaigupta004@gmail.com>
2024-01-08 00:00:04 +0530

    more utilities added

--- app/index.js
+++ app/index.js
@@ -27,19 +27,75 @@ app.post('/upload', async(req, res) => {
 
   const { localUploadPath, gcloudPath } = req.body;
 
-  await Data.uploadFile(localUploadPath, gcloudPath);
+  const result = await Data.uploadFile(localUploadPath, gcloudPath);
 
-  res.send('Upload Successful');
+  res.send(result);
 
 });
 
-app.post('/download', async(req, res) => {
+app.post('/download/memory', async(req, res) => {
 
   const { gcloudPath, localDownloadPath, } = req.body;
 
-  await Data.downloadFile(gcloudPath, localDownloadPath);
+  const result = await Data.downloadFileInMemory(gcloudPath, localDownloadPath);
 
-  res.send('Document updated successfully !');
+  res.send(result);
+
+});
+
+app.post('/download/RAM', async(req, res) => {
+
+  const { gcloudPath } = req.body;
+
+  const result = await Data.downloadFileInRAM(gcloudPath);
+
+  res.send(result);
+
+});
+
+app.post('/getmetadata', async(req, res) => {
+
+  const { gcloudPath } = req.body;
+
+  const [filename, data] = await Data.getMetadata(gcloudPath);
+
+  res.send({
+    filename: filename,
+      data: {...data}
+  });
+
+});
+
+app.post('/setmetadata', async(req, res) => {
+
+  const { gcloudPath, ...metadata } = req.body;
+
+  await Data.setMetadata(gcloudPath, metadata );
+
+  res.send({
+    filename: gcloudPath,
+      Metadata: {...metadata}
+  });
+
+});
+
+app.post('/update', async(req, res) => {
+
+  const { gcloudPath, data } = req.body;
+
+  const result = await Data.writeStream(gcloudPath, data);
+
+  res.send(result);
+
+});
+
+app.post('/read', async(req, res) => {
+
+  const { gcloudPath } = req.body;
+
+  const result = await Data.readStream(gcloudPath);
+
+  res.send(result);
 
 });
 
--- data/index.js
+++ data/index.js
@@ -7,22 +7,89 @@ const storage = new Storage({
 });
 
 const bucketName = 'test_bucket_zero65';
+const bucket = storage.bucket(bucketName);
 
 // Upload file from local machine to Google Cloud Storage
 async function uploadFile(localFilePath, remoteFileName) {
-  const bucket = storage.bucket(bucketName);
   await bucket.upload(localFilePath, {
     destination: remoteFileName,
   });
-  console.log(`File ${localFilePath} uploaded to ${bucketName}/${remoteFileName}.`);
+
+  return `File ${localFilePath} uploaded to ${bucketName}/${remoteFileName}.`;
 }
 
 // Download file from Google Cloud Storage to local machine
-async function downloadFile(remoteFileName, localFilePath) {
-  const bucket = storage.bucket(bucketName);
+async function downloadFileInMemory(remoteFileName, localFilePath) {
   const file = bucket.file(remoteFileName);
   await file.download({ destination: localFilePath });
-  console.log(`File ${bucketName}/${remoteFileName} downloaded to ${localFilePath}.`);
+
+  return `File ${bucketName}/${remoteFileName} downloaded to ${localFilePath}.`;
 }
 
-module.exports = {uploadFile, downloadFile}
+// Download file from Google Cloud Storage to RAM
+async function downloadFileInRAM(remoteFileName) {
+  const file = bucket.file(remoteFileName);
+  const [fileContent] = await file.download();
+
+  return `File ${bucketName}/${remoteFileName} downloaded to system RAM.`;
+}
+
+// Get Object Metadata of file in Google Cloud Storage 
+async function getMetadata(remoteFileName) {
+  const file = bucket.file(remoteFileName);
+  const [metadata] = await file.getMetadata();
+
+  return [remoteFileName, metadata];
+}
+
+// Function to set metadata for a file in Google Cloud Storage
+async function setMetadata(remoteFileName, metadata) {
+  const file = bucket.file(remoteFileName);
+
+  await file.setMetadata(metadata);
+  
+}
+
+// Writing data to file in Google Cloud Storage
+async function writeStream(remoteFileName, data) {
+  const file = bucket.file(remoteFileName);
+  
+  const writeStream = file.createWriteStream();
+  writeStream.write(data);
+  writeStream.end();
+
+  await new Promise((resolve, reject) => {
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
+  });
+
+  return `Data: ${data} \nupdated to ${bucketName}/${remoteFileName}.`
+}
+
+// Download data from Google Cloud Storage file using a readable stream
+async function readStream(remoteFileName) {
+  const file = bucket.file(remoteFileName);
+
+  const readStream = file.createReadStream();
+  let data = '';
+  readStream.on('data', (chunk) => {
+    data += chunk.toString();
+  });
+
+  await new Promise((resolve, reject) => {
+    readStream.on('end', resolve);
+    readStream.on('error', reject);
+  });
+
+  return `Data downloaded from ${bucketName}/${remoteFileName}: ${data}`;
+}
+
+module.exports = {
+   uploadFile, 
+   downloadFileInMemory, 
+   downloadFileInRAM, 
+   getMetadata, 
+   setMetadata, 
+   writeStream, 
+   readStream 
+  }
